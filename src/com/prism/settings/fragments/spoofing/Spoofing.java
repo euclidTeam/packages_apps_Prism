71170b9bece2755f50982e052d351031785e992e
AnierinB <anierin@evolution-x.org>
2025-10-18 23:55:50 +0530

    Prism: Add spoofing features  * Switch to json for PIF  * Restart dialog upon toggle  * Hide Pixel props switch on pixel 8+ devices  * Hide tensor switch on tensor devices
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Co-authored-by: Alvin Francis <nivlafx@gmail.com>
    Change-Id: I8cc3344711a0b86c3ca306ce9d5cedc10acd2a47
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    Signed-off-by: Mrick343 <mrick@euclidos.org>

--- src/com/prism/settings/fragments/spoofing/Spoofing.java
+++ src/com/prism/settings/fragments/spoofing/Spoofing.java
@@ -1,43 +1,313 @@
+/*
+ * Copyright (C) 2025 euclidOS
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package com.prism.settings.fragments.spoofing;
 
-import com.android.internal.logging.nano.MetricsProto;
-
-import android.os.Bundle;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
-import android.os.UserHandle;
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.ContentResolver;
+import android.content.Context;
 import android.content.res.Resources;
-import androidx.preference.ListPreference;
-import androidx.preference.Preference;
-import androidx.preference.PreferenceGroup;
-import androidx.preference.PreferenceScreen;
-import androidx.preference.PreferenceCategory;
-import androidx.preference.Preference.OnPreferenceChangeListener;
-import androidx.preference.PreferenceFragment;
-import androidx.preference.SwitchPreference;
-import android.provider.Settings;
-import com.android.settings.R;
-
-import java.util.Locale;
-import android.text.TextUtils;
-import android.view.View;
-
-import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.Utils;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemProperties;
 import android.util.Log;
+import android.widget.Toast;
 
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.internal.util.euclid.SystemRestartUtils;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.SearchIndexable;
+
+import com.euclid.support.preferences.SystemPropertySwitchPreference;
+import com.prism.settings.utils.Utils;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Iterator;
 import java.util.List;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.Collections;
 
+@SearchIndexable
 public class Spoofing extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener {
+
+    private static final String TAG = "Spoofing";
+
+    private static final String KEY_SYSTEM_WIDE_CATEGORY = "spoofing_system_wide_category";
+    private static final String KEY_PIF_JSON_FILE_PREFERENCE = "pif_json_file_preference";
+    private static final String KEY_GAME_PROPS_JSON_FILE_PREFERENCE = "game_props_json_file_preference";
+    private static final String KEY_UPDATE_JSON_BUTTON = "update_pif_json";
+
+    // System properties
+    private static final String SYS_GMS_SPOOF = "persist.sys.pixelprops.gms";
+    private static final String SYS_GOOGLE_SPOOF = "persist.sys.pphooks.enable";
+    private static final String SYS_GAMEPROP_SPOOF = "persist.sys.gameprops.enabled";
+    private static final String SYS_GPHOTOS_SPOOF = "persist.sys.gphooks.enable";
+    private static final String SYS_SNAP_SPOOF = "persist.sys.snap.enable";
+    private static final String SYS_VENDING_SPOOF = "persist.sys.vending.enable";
+    private static final String SYS_ENABLE_TENSOR_FEATURES = "persist.sys.features.tensor";
+
+    private Preference mGamePropsJsonFilePreference;
+    private Preference mPifJsonFilePreference;
+    private Preference mUpdateJsonButton;
+    private PreferenceCategory mSystemWideCategory;
+
+    private SystemPropertySwitchPreference mGmsSpoof;
+    private SystemPropertySwitchPreference mGoogleSpoof;
+    private SystemPropertySwitchPreference mGamePropsSpoof;
+    private SystemPropertySwitchPreference mGphotosSpoof;
+    private SystemPropertySwitchPreference mSnapSpoof;
+    private SystemPropertySwitchPreference mVendingSpoof;
+    private SystemPropertySwitchPreference mTensorFeaturesToggle;
+
+    private Handler mHandler;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mHandler = new Handler();
+        addPreferencesFromResource(R.xml.spoofing_settings);
+
+        final Context context = getContext();
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+        final Resources res = context.getResources();
+
+        getActivity().setTitle(R.string.prism_spoofing_dashboard_title);
+
+        mSystemWideCategory = findPreference(KEY_SYSTEM_WIDE_CATEGORY);
+        mGmsSpoof = findPreference(SYS_GMS_SPOOF);
+        mGoogleSpoof = findPreference(SYS_GOOGLE_SPOOF);
+        mGamePropsSpoof = findPreference(SYS_GAMEPROP_SPOOF);
+        mGphotosSpoof = findPreference(SYS_GPHOTOS_SPOOF);
+        mSnapSpoof = findPreference(SYS_SNAP_SPOOF);
+        mVendingSpoof = findPreference(SYS_VENDING_SPOOF);
+        mTensorFeaturesToggle = findPreference(SYS_ENABLE_TENSOR_FEATURES);
+
+        mPifJsonFilePreference = findPreference(KEY_PIF_JSON_FILE_PREFERENCE);
+        mGamePropsJsonFilePreference = findPreference(KEY_GAME_PROPS_JSON_FILE_PREFERENCE);
+        mUpdateJsonButton = findPreference(KEY_UPDATE_JSON_BUTTON);
+
+        String model = SystemProperties.get("ro.product.model");
+        boolean isTensorDevice = model.matches("Pixel [6-9][a-zA-Z ]*");
+
+        if (Utils.isCurrentlySupportedPixel()) {
+            mGoogleSpoof.setDefaultValue(false);
+            if (isMainlineTensorModel(model)) {
+                mSystemWideCategory.removePreference(mGoogleSpoof);
+            }
+        }
+
+        if (isTensorDevice) {
+            mSystemWideCategory.removePreference(mTensorFeaturesToggle);
+        }
+
+        mGmsSpoof.setOnPreferenceChangeListener(this);
+        mGoogleSpoof.setOnPreferenceChangeListener(this);
+        mGphotosSpoof.setOnPreferenceChangeListener(this);
+        mGamePropsSpoof.setOnPreferenceChangeListener(this);
+        mSnapSpoof.setOnPreferenceChangeListener(this);
+        mVendingSpoof.setOnPreferenceChangeListener(this);
+        mTensorFeaturesToggle.setOnPreferenceChangeListener(this);
+
+        mPifJsonFilePreference.setOnPreferenceClickListener(preference -> {
+            openFileSelector(10001);
+            return true;
+        });
+
+        mGamePropsJsonFilePreference.setOnPreferenceClickListener(preference -> {
+            openFileSelector(10002);
+            return true;
+        });
+
+        mUpdateJsonButton.setOnPreferenceClickListener(preference -> {
+            updatePropertiesFromUrl("https://raw.githubusercontent.com/euclidTeam/vendor_euclid/refs/heads/16/PlayIntegrity/pif.json");
+            return true;
+        });
+
+        Preference showPropertiesPref = findPreference("show_pif_properties");
+        if (showPropertiesPref != null) {
+            showPropertiesPref.setOnPreferenceClickListener(preference -> {
+                showPropertiesDialog();
+                return true;
+            });
+        }
+    }
+
+    private boolean isMainlineTensorModel(String model) {
+        return model.matches("Pixel [8-9][a-zA-Z ]*");
+    }
+
+    private void openFileSelector(int requestCode) {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("application/json");
+        startActivityForResult(intent, requestCode);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK && data != null) {
+            Uri uri = data.getData();
+            if (uri != null) {
+                if (requestCode == 10001) {
+                    loadPifJson(uri);
+                } else if (requestCode == 10002) {
+                    loadGameSpoofingJson(uri);
+                }
+            }
+        }
+    }
+
+    private void showPropertiesDialog() {
+        StringBuilder properties = new StringBuilder();
+        try {
+            JSONObject jsonObject = new JSONObject();
+            String[] keys = {
+                    "persist.sys.pihooks_ID",
+                    "persist.sys.pihooks_BRAND",
+                    "persist.sys.pihooks_DEVICE",
+                    "persist.sys.pihooks_FINGERPRINT",
+                    "persist.sys.pihooks_MANUFACTURER",
+                    "persist.sys.pihooks_MODEL",
+                    "persist.sys.pihooks_PRODUCT",
+                    "persist.sys.pihooks_SECURITY_PATCH",
+                    "persist.sys.pihooks_DEVICE_INITIAL_SDK_INT"
+            };
+            for (String key : keys) {
+                String value = SystemProperties.get(key, null);
+                if (value != null) {
+                    String buildKey = key.replace("persist.sys.pihooks_", "");
+                    jsonObject.put(buildKey, value);
+                }
+            }
+            properties.append(jsonObject.toString(4));
+        } catch (JSONException e) {
+            Log.e(TAG, "Error creating JSON from properties", e);
+            properties.append(getString(R.string.error_loading_properties));
+        }
+
+        new AlertDialog.Builder(getContext())
+                .setTitle(R.string.show_pif_properties_title)
+                .setMessage(properties.toString())
+                .setPositiveButton(android.R.string.ok, null)
+                .show();
+    }
+
+    private void updatePropertiesFromUrl(String urlString) {
+        new Thread(() -> {
+            try {
+                URL url = new URL(urlString);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                try (InputStream inputStream = connection.getInputStream()) {
+                    String json = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+                    JSONObject jsonObject = new JSONObject(json);
+                    String spoofedModel = jsonObject.optString("MODEL", "Unknown model");
+                    for (Iterator<String> it = jsonObject.keys(); it.hasNext(); ) {
+                        String key = it.next();
+                        String value = jsonObject.getString(key);
+                        SystemProperties.set("persist.sys.pihooks_" + key, value);
+                    }
+                    mHandler.post(() -> Toast.makeText(getContext(),
+                            getString(R.string.toast_spoofing_success, spoofedModel),
+                            Toast.LENGTH_LONG).show());
+                } finally {
+                    connection.disconnect();
+                }
+            } catch (Exception e) {
+                Log.e(TAG, "Error downloading JSON or setting properties", e);
+                mHandler.post(() -> Toast.makeText(getContext(),
+                        R.string.toast_spoofing_failure, Toast.LENGTH_LONG).show());
+            }
+            mHandler.postDelayed(() -> SystemRestartUtils.showSystemRestartDialog(getContext()), 1250);
+        }).start();
+    }
+
+    private void loadPifJson(Uri uri) {
+        try (InputStream inputStream = getActivity().getContentResolver().openInputStream(uri)) {
+            if (inputStream != null) {
+                String json = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+                JSONObject jsonObject = new JSONObject(json);
+                for (Iterator<String> it = jsonObject.keys(); it.hasNext(); ) {
+                    String key = it.next();
+                    String value = jsonObject.getString(key);
+                    SystemProperties.set("persist.sys.pihooks_" + key, value);
+                }
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Error reading PIF JSON", e);
+        }
+        mHandler.postDelayed(() -> SystemRestartUtils.showSystemRestartDialog(getContext()), 1250);
+    }
+
+    private void loadGameSpoofingJson(Uri uri) {
+        try (InputStream inputStream = getActivity().getContentResolver().openInputStream(uri)) {
+            if (inputStream != null) {
+                String json = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+                JSONObject jsonObject = new JSONObject(json);
+                for (Iterator<String> it = jsonObject.keys(); it.hasNext(); ) {
+                    String key = it.next();
+                    if (key.startsWith("PACKAGES_") && !key.endsWith("_DEVICE")) {
+                        String deviceKey = key + "_DEVICE";
+                        if (jsonObject.has(deviceKey)) {
+                            JSONObject deviceProps = jsonObject.getJSONObject(deviceKey);
+                            JSONArray packages = jsonObject.getJSONArray(key);
+                            for (int i = 0; i < packages.length(); i++) {
+                                setGameProps(packages.getString(i), deviceProps);
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Error reading Game Props JSON", e);
+        }
+        mHandler.postDelayed(() -> SystemRestartUtils.showSystemRestartDialog(getContext()), 1250);
+    }
+
+    private void setGameProps(String packageName, JSONObject deviceProps) {
+        try {
+            for (Iterator<String> it = deviceProps.keys(); it.hasNext(); ) {
+                String key = it.next();
+                String value = deviceProps.getString(key);
+                SystemProperties.set("persist.sys.gameprops." + packageName + "." + key, value);
+            }
+        } catch (JSONException e) {
+            Log.e(TAG, "Error parsing device properties", e);
+        }
+    }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mGmsSpoof
+                || preference == mGoogleSpoof
+                || preference == mGphotosSpoof
+                || preference == mGamePropsSpoof
+                || preference == mSnapSpoof
+                || preference == mVendingSpoof) {
+            SystemRestartUtils.showSystemRestartDialog(getContext());
+            return true;
+        }
+        if (preference == mTensorFeaturesToggle) {
+            boolean enabled = (Boolean) newValue;
+            SystemProperties.set(SYS_ENABLE_TENSOR_FEATURES, enabled ? "true" : "false");
+            SystemRestartUtils.showSystemRestartDialog(getContext());
+            return true;
+        }
         return false;
     }
 
@@ -46,11 +316,6 @@ public class Spoofing extends SettingsPreferenceFragment implements Preference.O
         return MetricsProto.MetricsEvent.PRISM;
     }
 
-    @Override
-    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-        setPreferencesFromResource(R.xml.spoofing_settings, rootKey);
-
-        getActivity().setTitle(R.string.prism_spoofing_dashboard_title);
-
-    }
+    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider(R.xml.spoofing_settings);
 }
---
+++ src/com/prism/settings/utils/Utils.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2016 The Pure Nexus Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.prism.settings.utils;
+
+import static android.os.UserHandle.USER_SYSTEM;
+
+import android.content.Context;
+import android.content.om.IOverlayManager;
+import android.os.AsyncTask;
+import android.os.RemoteException;
+import android.os.SystemProperties;
+import android.widget.Toast;
+import com.android.settings.R;
+
+import java.util.Arrays;
+
+public class Utils {
+
+    public static boolean isCurrentlySupportedPixel() {
+         boolean isPixelDevice = SystemProperties.get("ro.product.model").matches("Pixel [3-9][a-zA-Z ]*");
+         return isPixelDevice;
+     }
+}
